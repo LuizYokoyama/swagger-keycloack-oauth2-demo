1a9c87068e1473b810c41f344022b79c92e0e54b
luiz <luizyfs@gmail.com>
2023-06-23 10:11:54 -0300

    3º commit

--- src/main/java/io/github/luizyokoyama/swaggerkeycloackoauth2demo/controller/Controller.java
+++ src/main/java/io/github/luizyokoyama/swaggerkeycloackoauth2demo/controller/Controller.java
@@ -2,6 +2,8 @@ package io.github.luizyokoyama.swaggerkeycloackoauth2demo.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
 
     @Operation(summary = "Get a info by its id")
+    @ApiResponses({ @ApiResponse(responseCode = "200", description = "The infos were founded and returned."),
+                    @ApiResponse(responseCode = "401", description = "The request requires authentication"),
+                    @ApiResponse(responseCode = "403", description = "Authority privilege of read not provided")})
     @GetMapping("info/{id}")
     public String getById(@Parameter(description = "id of info to be searched") @PathVariable long id) {
         return "Info "+id+" OK";
@@ -18,6 +23,9 @@ public class Controller {
     }
 
     @Operation(summary = "Post a info by its id")
+    @ApiResponses({ @ApiResponse(responseCode = "200", description = "The infos were posted and returned."),
+                    @ApiResponse(responseCode = "401", description = "The request requires authentication"),
+                    @ApiResponse(responseCode = "403", description = "Authority privilege of write not provided")})
     @PostMapping("info/{id}")
     public String postById(@Parameter(description = "id of info to be posted") @PathVariable long id) {
         return "Info "+id+" OK";
