42df27090c8a66c4a365b319c572a661384d49a0
luiz <luizyfs@gmail.com>
2023-07-10 19:58:20 -0300

    5º commit

--- src/main/java/io/github/luizyokoyama/swaggerkeycloackoauth2demo/config/SecurityConfig.java
+++ src/main/java/io/github/luizyokoyama/swaggerkeycloackoauth2demo/config/SecurityConfig.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
 import org.springframework.security.core.GrantedAuthority;
@@ -16,7 +15,6 @@ import java.util.Collection;
 import java.util.Map;
 
 @Configuration
-@EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
